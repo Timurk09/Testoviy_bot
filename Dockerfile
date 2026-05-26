a5bdef4910a4437ffff7e607386d536b106b5803
Timurka <spicin.t@gmail.com>
2026-05-27 01:23:08 +0700

    Tst
    Ne_discription_tut_mne

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:10.0 AS base
+USER $APP_UID
+WORKDIR /app
+EXPOSE 8080
+EXPOSE 8081
+
+FROM mcr.microsoft.com/dotnet/sdk:10.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["Tst_bot/Tst_bot.csproj", "Tst_bot/"]
+RUN dotnet restore "Tst_bot/Tst_bot.csproj"
+COPY . .
+WORKDIR "/src/Tst_bot"
+RUN dotnet build "./Tst_bot.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./Tst_bot.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Tst_bot.dll"]
---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Tst_bot.Database;
+//using NgrokAspNetCore;
+using Telegram.Bot;
+
+var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsProduction())
+{
+    var port = Environment.GetEnvironmentVariable("PORT");
+    if (!string.IsNullOrEmpty(port) && int.TryParse(port, out var portNumber))
+    {
+        builder.WebHost.UseUrls($"http://0.0.0.0:{portNumber}");
+    }
+}
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbcontext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddNgrok();
+var botToken = builder.Configuration["Telegram:BotToken"]
+    ?? throw new InvalidOperationException("Telegram:BotToken is not set!");
+builder.Services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(botToken!));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
